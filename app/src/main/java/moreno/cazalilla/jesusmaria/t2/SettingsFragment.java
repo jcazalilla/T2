f09ea515b942acaf9bb216d8670a8fd0d4ce17c9
jesus <jcazalilla@gmail.com>
2024-11-27 09:55:34 +0100

    Tarea 2 PMDM- peso detailfragment por corregir

--- app/src/main/java/moreno/cazalilla/jesusmaria/t2/SettingsFragment.java
+++ app/src/main/java/moreno/cazalilla/jesusmaria/t2/SettingsFragment.java
@@ -5,15 +5,19 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.preference.PreferenceFragmentCompat;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import moreno.cazalilla.jesusmaria.t2.databinding.SettingsFragmentBinding;
+
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
+    private SettingsFragmentBinding binding;
 
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
@@ -26,7 +30,12 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         super.onStart();
         //cambia el título del ActionBar
         if (getActivity() != null) {
-            ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Ajustes");
+            ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.settings);
         }
+    }
+
+    private void changeToListFragment(View view) {
+        //navegamos al fragmento listado de personajes
+        Navigation.findNavController(view).navigate(R.id.personajeListFragment);
     }
 }
